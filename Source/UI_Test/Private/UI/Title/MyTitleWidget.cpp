dd09469613abaa48e0fa3b12a25a9412124abfdc
zeltina99 <minec369@outlook.com>
2026-01-26 16:35:25 +0900

    커밋

---
+++ Source/UI_Test/Private/UI/Title/MyTitleWidget.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Title/MyTitleWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h" // 레벨 이동(OpenLevel)을 위해 필요
+
+void UMyTitleWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 1. 버튼 이벤트 바인딩
+	if (ScreenTouchBtn)
+	{
+		// 버튼이 클릭되면 OnScreenTouched 함수를 실행하도록 연결
+		ScreenTouchBtn->OnClicked.AddDynamic(this, &UMyTitleWidget::OnScreenTouched);
+	}
+
+	// 2. 깜빡임 애니메이션 재생
+	if (BlinkAnim)
+	{
+		// PlayAnimation(애니메이션, 시작시간, 반복횟수, 재생모드, 재생속도)
+		// 반복 횟수(NumLoopsToPlay)가 0이면 무한 반복입니다.
+		PlayAnimation(BlinkAnim, 0.0f, 0);
+	}
+}
+
+void UMyTitleWidget::OnScreenTouched()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), FName("Lobby"));
+}
---
+++ Source/UI_Test/Public/UI/Title/MyTitleWidget.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyTitleWidget.generated.h"
+
+class UButton;
+class UWidgetAnimation;	// 애니메이션용 전방 선언
+
+/**
+ * @class UMyTitleWidget
+ * @brief 게임의 타이틀 화면을 관리하는 위젯 클래스입니다.
+ * @note 화면 전체를 덮는 투명 버튼을 통해 'Touch to Start'기능을 구현하며,
+ * 텍스트 깜빡임 애니메이션을 재생합니다.
+ */
+UCLASS()
+class UI_TEST_API UMyTitleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	/**
+	* @brief 위젯이 화면에 생성된 직후 호출되는 초기화 함수입니다.
+	* @note 여기서 버튼 이벤트를 바인딩하고, 초기 애니메이션을 재생합니다.
+	*/
+	virtual void NativeConstruct() override;
+
+private:
+	/**
+	* @brief 화면 전체를 덮는 투명 버튼입니다.
+	* @note meta = (BindWidget) 속성은 언리얼 에디터의 위젯 이름과 변수명이 일치해야 자동으로 연결됩니다.
+	*/
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ScreenTouchBtn = nullptr;
+
+	/**
+	* @brief 'Touch to Start' 텍스트를 깜빡이게 하는 애니메이션입니다.
+	* @note Transient 키워드는 이 변수가 디스크에 저장(Serialize)될 필요가 없음을 의미합니다.
+	* meta = (BindWidgetAnim)을 통해 에디터의 애니메이션 트랙과 연결됩니다.
+	*/
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> BlinkAnim = nullptr;
+	
+	/**
+	* @brief 화면(투명 버튼)을 터치했을 때 호출되는 콜백 함수입니다.
+	* @note 로비 레벨로 이동하는 로직을 수행합니다.
+	*/
+	UFUNCTION()
+	void OnScreenTouched();
+
+};
