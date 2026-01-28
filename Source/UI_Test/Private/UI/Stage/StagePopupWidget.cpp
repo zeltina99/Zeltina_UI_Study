a55d4db5f0796a404c66070cbb181a70949df770
zeltina99 <minec369@outlook.com>
2026-01-28 16:31:32 +0900

    스테이지 팝업창 설계

--- Source/UI_Test/Private/UI/Stage/StagePopupWidget.cpp
+++ Source/UI_Test/Private/UI/Stage/StagePopupWidget.cpp
@@ -2,4 +2,58 @@
 
 
 #include "UI/Stage/StagePopupWidget.h"
+#include "Framework/MyGameInstance.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
 
+void UStagePopupWidget::UpdatePopupInfo(const FStageData& Data)
+{
+	if (TitleText)
+	{
+		TitleText->SetText(Data.StageName);
+	}
+	if (DescText)
+	{
+		DescText->SetText(Data.Description);
+	}
+
+	//입장할 레벨 이름 캐싱
+	CurrentLevelName = Data.LevelToLoad;
+
+	// TODO: PreviewImage에 Data.Thumnail 비동기 로딩 적용 필요
+	// SetBrushFromSoftTexture 등을 활용하여 UI 블로킹 방지
+}
+
+void UStagePopupWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 입장 버튼
+	if (EnterBtn)
+	{
+		EnterBtn->OnClicked.AddDynamic(this, &UStagePopupWidget::OnEnterClicked);
+	}
+	// 닫기 버튼
+	if (CloseBtn)
+	{
+		CloseBtn->OnClicked.AddDynamic(this, &UStagePopupWidget::OnCloseClicked);
+	}
+}
+
+void UStagePopupWidget::OnEnterClicked()
+{
+	// GameInstance를 통해 비동기 에셋 로딩 및 레벨 이동 요청
+	if (UMyGameInstance* GI = Cast<UMyGameInstance>(GetGameInstance()))
+	{
+		// 빈 배열을 넘기거나, 로딩이 필요한 추가 에셋 리스트를 넘김
+		TArray<TSoftObjectPtr<UObject>> AssetsToLoad;
+		GI->OpenLevelWithAsyncLoad(CurrentLevelName, AssetsToLoad);
+	}
+}
+
+void UStagePopupWidget::OnCloseClicked()
+{
+	// 팝업 닫기 (Viewport에서 제거)
+	RemoveFromParent();
+}
--- Source/UI_Test/Public/UI/Stage/StagePopupWidget.h
+++ Source/UI_Test/Public/UI/Stage/StagePopupWidget.h
@@ -4,14 +4,72 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Framework/Data/FStageData.h"
 #include "StagePopupWidget.generated.h"
 
+class UTextBlock;
+class UImage;
+class UButton;
+
 /**
- * 
+ * @class UStagePopupWidget
+ * @brief 스테이지 선택 시 상세 정보를 표시하고 입장을 처리하는 팝업 위젯입니다.
  */
 UCLASS()
 class UI_TEST_API UStagePopupWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	/**
+	 * @brief 데이터 테이블에서 읽어온 정보를 바탕으로 UI를 갱신합니다.
+	 * @param Data 표시할 스테이지 데이터 구조체
+	 */
+	UFUNCTION(BlueprintCallable, Category = "UI|Logic")
+	void UpdatePopupInfo(const FStageData& Data);
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	/**
+	 * @brief 스테이지 제목 텍스트
+	 */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TitleText = nullptr;
+	/**
+	 * @brief 스테이지 설명 텍스트
+	 */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> DescText = nullptr;
+	/**
+	 * @brief 스테이지 미리보기 이미지
+	 */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PreviewImage = nullptr;
+	/**
+	 * @brief 게임 시작(입장) 버튼
+	 */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> EnterBtn = nullptr;
+	/**
+	 * @brief 팝업 닫기 버튼
+	 */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CloseBtn = nullptr;
+	/**
+	 * @brief 임장 버튼 클릭 시 사용할 레벨 이름입니다.
+	 * @details UpdatePopupInfo 호출 시 캐싱됩니다.
+	 */
+	FName CurrentLevelName;
+	/**
+	 * @brief 입장 버튼 클릭 핸들러. 비동기 로딩을 요청합니다.
+	 */
+	UFUNCTION()
+	void OnEnterClicked();
+	/**
+	 * @brief 닫기 버튼 클릭 핸들러. 비동기 로딩을 요청합니다.
+	 */
+	UFUNCTION()
+	void OnCloseClicked();
 };
