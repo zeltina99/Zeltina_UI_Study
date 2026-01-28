1279f9ac9654f87c072ee041acb3e6857a19d977
zeltina99 <minec369@outlook.com>
2026-01-28 16:12:55 +0900

    스테이지 노드 설계

--- Source/UI_Test/Private/UI/Stage/StageNodeWidget.cpp
+++ Source/UI_Test/Private/UI/Stage/StageNodeWidget.cpp
@@ -8,12 +8,49 @@
 
 void UStageNodeWidget::InitializeNode(bool bIsLocked)
 {
+	if (bIsLocked)
+	{
+		// 잠금 상태 처리
+		StageBtn->SetIsEnabled(false);
+		if (LockIcon)
+		{
+			LockIcon->SetVisibility(ESlateVisibility::Visible);
+		}
+		// 버튼 틴트를 어둡게 변경하여 비활성화 느낌 강조
+		StageBtn->SetBackgroundColor(FLinearColor(0.3f, 0.3f, 0.3f, 1.0f));
+	}
+	else
+	{
+		// 해금 상태 처리
+		StageBtn->SetIsEnabled(true);
+		if (LockIcon)
+		{
+			LockIcon->SetVisibility(ESlateVisibility::Hidden);
+		}
+		StageBtn->SetBackgroundColor(FLinearColor::White);
+	}
+
+	if (StageNumberText)
+	{
+		StageNumberText->SetText(FText::AsNumber(TargetStageIndex));
+	}
 }
 
 void UStageNodeWidget::NativeConstruct()
 {
+	Super::NativeConstruct();
+
+	if (StageBtn)
+	{
+		StageBtn->OnClicked.AddDynamic(this, &UStageNodeWidget::OnBtnClicked);
+	}
 }
 
 void UStageNodeWidget::OnBtnClicked()
 {
+	// 델리게이트가 바인딩되어 있다면, 자신의 인덱스를 실어 방송
+	if (OnStageSelected.IsBound())
+	{
+		OnStageSelected.Broadcast(TargetStageIndex);
+	}
 }
